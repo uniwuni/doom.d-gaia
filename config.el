2e28baf2db4c3748b066d743d1e8eac0c482129c
uni marx <uniwuni@protonmail.com>
2025-07-11 17:12:56 +0200

    cool roam graph stuff

--- config.el
+++ config.el
@@ -102,3 +102,21 @@
 (after! org (setq org-support-shift-select t)
         (advice-add #'org-agenda :before (lambda (&rest _args) (setq org-agenda-files (my/org-get-agenda-files))))
   )
+
+
+; https://github.com/org-roam/org-roam-ui
+
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
