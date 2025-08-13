da936977742734aa6316074307b470a14501c517
Uni Marx <uniwuni@protonmail.com>
2025-08-13 14:09:33 +0200

    scartcl

--- config.el
+++ config.el
@@ -299,3 +299,13 @@ holding contextual information."
     (insert (concat "[[id:" id "][" title "]] :proof:" "\n"))
     (org-id-get-create)
     ))
+
+;; koma articles
+(with-eval-after-load "ox-latex"
+  (add-to-list 'org-latex-classes
+               '("koma-article" "\\documentclass{scrartcl}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
